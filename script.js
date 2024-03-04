c67c53aa11ce080ebfb790a31b34fbb8c00c7076
aish <aishnikam31@gmail.com>
2024-03-05 00:03:15 +0530

    style file fixed

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
-
-
-
 function updateOutput() {
-    const editorContent = document.getElementById('editor').querySelector('textarea').value;
+    const editorContent = document.getElementById('codeInput').value;
     const outputFrame = document.getElementById('outputFrame');
     const output = outputFrame.contentDocument || outputFrame.contentWindow.document;
 
-    // outputFrame.contentDocument: This part is trying to access the contentDocument property of the iframe. The contentDocument property represents the document content of the iframe. This property is supported in most modern browsers.
-
-// outputFrame.contentWindow.document: In case the contentDocument property is not supported (for example, in older versions of Internet Explorer), the code falls back to accessing the document through the contentWindow.document property. The contentWindow property represents the window inside the iframe, and from there, document is accessed.
-
-// By using this line of code, you're attempting to get a reference to the document within the outputFrame, and the output variable will hold whichever one of these options is available and supported by the browser.
-
     output.open();
-    // This method is called to open the document for writing. It is used to prepare the document for receiving new content.
     output.write(editorContent);
-    // output.write(editorContent): This line writes the content of the editorContent (the value of the textarea) into the output document. This is where the actual update of the output content occurs.
     output.close();
-    // output.close(): Finally, this method is called to close the document. This step is important to complete the writing process and make sure the changes take effect.
+
+    // Update line numbers based on visible lines
+    updateVisibleLineNumbers();
 }
 
- let clear = document.getElementById('clear');
- clear.addEventListener('click', function(){
-    document.getElementById('editor').querySelector('textarea').value = '';
-    updateOutput();
-    // why updateOutput is called here? 
-
- })
-
-
- function updateLineNumbers() {
+function updateVisibleLineNumbers() {
     var codeInput = document.getElementById("codeInput");
     var lineNumbers = document.getElementById("lineNumbers");
+
+    var scrollTop = codeInput.scrollTop;
+    var lineHeight = codeInput.scrollHeight / codeInput.rows;
+    var firstVisibleLineNumber = Math.floor(scrollTop / lineHeight) + 1;
 
     var lines = codeInput.value.split("\n");
     var lineNumberContent = "";
 
-    for (var i = 1; i <= lines.length; i++) {
+    for (var i = firstVisibleLineNumber; i <= lines.length; i++) {
         lineNumberContent += i + "<br>";
     }
 
-    lineNumbers.innerHTML = lineNumberContent;
+    lineNumbers.innerHTML = "<pre>" + lineNumberContent + "</pre>";
+}
+
+function syncScroll() {
+    var codeInput = document.getElementById("codeInput");
+    var lineNumbers = document.getElementById("lineNumbers");
+
+    lineNumbers.scrollTop = codeInput.scrollTop;
 }
 
 // Initial line numbers update
-updateLineNumbers();
+updateVisibleLineNumbers();
+
+
+let clear = document.getElementById('clear');
+clear.addEventListener('click', function(){
+document.getElementById('editor').querySelector('textarea').value = '';
+updateOutput();
+// why updateOutput is called here? 
+
+})
